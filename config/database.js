e93778669d8cf994ad974184c2b7c9b394e4a8b3
eduviana <eduardoviana83@gmail.com>
2022-12-29 12:34:54 -0300

    env fix

--- config/database.js
+++ config/database.js
@@ -12,20 +12,36 @@
 //   },
 // });
 
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', 'ep-shrill-shadow-577905.cloud.neon.tech'),
+//       database: env('DATABASE_NAME', 'neondb'),
+//       user: env('DATABASE_USERNAME', 'eduardoviana83'),
+//       password: env('DATABASE_PASSWORD', 'W7LtNkPfjwi2'),
+//       ssl: env.bool('DATABASE_SSL', true),
+//     },
+//   },
+// });
+
+
+
+
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'ep-shrill-shadow-577905.cloud.neon.tech'),
-      database: env('DATABASE_NAME', 'neondb'),
-      user: env('DATABASE_USERNAME', 'eduardoviana83'),
-      password: env('DATABASE_PASSWORD', 'W7LtNkPfjwi2'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: env('DATABASE_HOST'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL'),
     },
   },
 });
-
-
 
 
 
